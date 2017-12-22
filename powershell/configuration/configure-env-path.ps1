7fb6f3094a61203dc5652e38a6d0acc53e77f6a2
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2017-12-22 15:11:15 +0100

    Update configure-env-path.ps1

--- powershell/configuration/configure-env-path.ps1
+++ powershell/configuration/configure-env-path.ps1
@@ -3,10 +3,13 @@ function Test-Env ($path)
 	return $env:path -split ";" -contains $path
 }
 
-function Get-FolderInProgramFiles() {
-    param ( [string]$folder = $(throw "Please secify searched folder within program files"))
+function Get-FolderIn() {
+    param ( 
+	[string]$where = $(throw "Please secify path where search"),
+	[string]$folder = $(throw "Please secify searched folder within program files")
+	)
 
-    return ls env: | ? Name -match ^ProgramFiles | ForEach-Object { join-path $_.Value $folder } | Where-Object { Test-Path $_ }
+    return ls env: | ? Name -match ^$where | ForEach-Object { join-path $_.Value $folder } | Where-Object { Test-Path $_ }
 }
 
 function Set-To-Env-Path($path)
@@ -19,12 +22,12 @@ Set-To-Env-Path "$profilePath\Scripts"
 
 #12.0 because I have VS 2013
 #-1 because MSbuild exists in both directories and I want to get x86(last directory)
-$msbuildPath = (Get-FolderInProgramFiles "MSBuild")[-1]
+$msbuildPath = (Get-FolderIn "ProgramFiles" "MSBuild")[-1]
 Set-To-Env-Path "$msbuildPath\12.0\Bin"
 Set-To-Env-Path "$msbuildPath\14.0\Bin"
 Set-To-Env-Path "$msbuildPath\4.0\Bin"
 
-$gitPath = (Get-FolderInProgramFiles "Git")
+$gitPath = (Get-FolderIn "LOCALAPPDATA" "Programs\Git")
 if ($gitPath)
 {
 	Set-To-Env-Path "$gitPath\minigw\bin"
@@ -34,6 +37,6 @@ if ($gitPath)
 }
 else { write-host "There is not Git in ProgramFiles. Please install" -ForegroundColor Red }
 
-$gnuplotPath = (Get-FolderInProgramFiles "gnuplot")
+$gnuplotPath = (Get-FolderIn "ProgramFiles" "gnuplot")
 Set-To-Env-Path "$gnuplotPath\bin"
 Set-To-Env-Path "$env:LOCALAPPDATA\Microsoft\dotnet"
