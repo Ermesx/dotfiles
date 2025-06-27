6ff8fa77367af228448b20288650783891a36dc7
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-06-27 18:40:31 +0200

    add build scripts and first touch to vagrant

---
+++ build.ps1
@@ -0,0 +1,44 @@
+﻿$ErrorActionPreference = "Stop"
+
+# Define the root directories
+$sourceRoot = "src"
+$destination = "build/windows"
+
+# Exclude files with the
+$excludedExtension = ".sh"
+$excludedKeyword = "macos"
+
+Write-Host "🚀 Start build for Windows in '$destination'"
+
+# Reset the build folder if it already exists
+if (Test-Path $destination) {
+    Remove-Item -Recurse -Force $destination
+}
+New-Item -ItemType Directory -Path $destination | Out-Null
+
+# Get all files from the src directory (excluding .sh files and files containing 'macos' in the name)
+$allFiles = Get-ChildItem -Path $sourceRoot -Recurse -File | Where-Object {
+    ($_.Extension.ToLower() -ne $excludedExtension) -and ($_.FullName.ToLower() -notlike "*$excludedKeyword*")
+}
+
+$totalFiles = $allFiles.Count
+$counter = 0
+
+foreach ($item in $allFiles) {
+    # Determine the target path based on the source path
+    $relativePath = $item.FullName -replace ".*\\$sourceRoot\\", ""
+    $target = Join-Path $destination $relativePath
+    $targetDir = Split-Path -Path $target -Parent
+    
+    if (!(Test-Path $targetDir)) {
+        New-Item -ItemType Directory -Path $targetDir -Force | Out-Null
+    }
+    
+    Write-Host " => Copying file: $($relativePath)"
+    Copy-Item -Path $item.FullName -Destination $target -Force
+    
+    $counter++
+    Write-Progress -Activity "Copying common and windows files" -Status "$counter of $totalFiles" -PercentComplete (($counter / $totalFiles) * 100)
+}
+
+Write-Host "✅ Build for Windows completed in '$destination'"
---
+++ src/windows/powershell/profile_common.ps1
@@ -0,0 +1,55 @@
+
+# General variables
+$profileRoot    = "$(split-path $profile)"
+$scripts     	= "$profileRoot\Scripts"
+$configuration	= "$profileRoot\configuration"
+
+# Configure PATH
+. $configuration\configure-env-path.ps1
+
+# Imports
+Import-Module PowerTab -ArgumentList "$profileRoot\PowerTabConfig.xml"
+
+Import-Module posh-git
+Enable-GitColors
+
+Import-Module posh-rake
+
+if ($PSVersionTable.PSVersion.Major -lt 5)
+{
+	. $configuration\PSReadLine\PSReadlineConfig.ps1
+}
+
+# Configure prompt
+. $configuration\configure-ps-prompt.ps1
+
+# add function
+. $scripts\git-most-changed.ps1
+
+# Configure ssh-agent so git doesn't require a password on every push
+. $scripts\ssh-agent-utils.ps1
+
+# return all IP addresses
+function get-ips() {
+   $ent = [net.dns]::GetHostEntry([net.dns]::GetHostName())
+   return $ent.AddressList | ?{ $_.ScopeId -ne 0 } | %{
+      [string]$_
+   }
+}
+
+# save last 100 history items on exit
+$historyPath = Join-Path (split-path $profile) history.clixml
+ 
+# hook powershell's exiting event & hide the registration with -supportevent.
+Register-EngineEvent -SourceIdentifier powershell.exiting -SupportEvent -Action {
+    Get-History -Count 20 | Export-Clixml (Join-Path (split-path $profile) history.clixml) }
+ 
+# load previous history, if it exists
+if ((Test-Path $historyPath)) {
+    Import-Clixml $historyPath | ? {$count++;$true} | Add-History
+    Write-Host -Fore Green "`nLoaded $count history item(s).`n"
+}
+
+. $profileRoot\Scripts\Set-ConsoleIcon.ps1
+
+[GC]::Collect()
