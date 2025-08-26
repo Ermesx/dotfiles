147e456ef05bd456f5deae45a5c84f254819d7d0
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-08-27 01:29:07 +0200

    Refactor Installed-Apps-Modules.Tests.ps1 to use script-scoped variables and improve module installation checks; add env-variables_profile.ps1.tmpl for environment variable management

--- tests/windows/Installed-Apps-Modules.Tests.ps1
+++ tests/windows/Installed-Apps-Modules.Tests.ps1
@@ -1,25 +1,24 @@
 ﻿#Requires -Modules Pester
-
 Describe "After installing dotfiles" {
-
+    
     BeforeAll {
         $script:allApps = Get-WinGetPackage
         $script:allModules = Get-Module -ListAvailable
         $packagesPath = Join-Path $HOME ".local\share\chezmoi\.home\dot_config\winget-dsc\packages.dsc.winget"
         $resources = (Get-Content -Path $packagesPath | ConvertFrom-Yaml).properties.resources
-        $wingets = $resources | Where-Object { $_.resource -eq 'Microsoft.WinGet.DSC/WinGetPackage' }
-        $modules = $resources | Where-Object { $_.resource -eq 'PowerShellModule/PSModuleResource' }
+        $script:wingets = $resources | Where-Object { $_.resource -eq 'Microsoft.WinGet.DSC/WinGetPackage' }
+        $script:modules = $resources | Where-Object { $_.resource -eq 'PowerShellModule/PSModuleResource' }
     }
     
     Context "Apps" {
-        It "should have installed <_>" -Foreach $wingets {
+        It "should have installed <_>" -Foreach $script:wingets {
             $id = $_
-            $script:allApps | Where-Object { $_.Id -like "*$id*" } | Should -Not -BeNullOrEmpty
+            $script:allApps | Where-Object { $_.Id -eq $id } | Should -Not -BeNullOrEmpty
         }
     }
     
     Context "Modules" {
-        It "should have installed <_>" -Foreach $modules {
+        It "should have installed <_>" -Foreach $script:modules {
             $name = $_
             $script:allModules | Where-Object { $_.Name -eq $name } | Should -Not -BeNullOrEmpty
         }
