0e0768f299d6bb152b7bb56cf5cfce6daa29eec8
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-08-26 20:47:00 +0200

    Refactor install.ps1 to replace Write-Output with Write-Host for output messages and ensure destination directory creation

--- install.ps1
+++ install.ps1
@@ -11,7 +11,7 @@ function Download-File {
     )
     $name = Split-Path -Path $Url -Leaf
 
-    Write-Output "► Downloading $name" -ForegroundColor Cyan
+    Write-Host "► Downloading $name" -ForegroundColor Cyan
     Invoke-RestMethod -Uri $Url -OutFile $Destination
 }
 
@@ -26,21 +26,26 @@ function Get-EnvVar {
 # Resrouces
 $GITHUB_URL = "https://raw.githubusercontent.com/$GITHUB_USERNAME/dotfiles/refs/heads/$BRANCH/home/dot_config/winget-dsc"
 $wingets = @('packages.dsc.winget', 'windows.dsc.winget')
-$dest = "~\.config\winget-dsc"
+$dest = Join-Path $HOME ".config\winget-dsc"
 
 if (Get-Command -Name winget -ErrorAction SilentlyContinue) {
+    # Create destination directory if it doesn't exist
+    if (-Not (Test-Path -Path $dest)) {
+        New-Item -ItemType Directory -Path $dest | Out-Null
+    }
+    
     # Download winget-dsc configuration files
     $wingets | Foreach-Object { Download-File -Url "$GITHUB_URL/$_" -Destination (Join-Path $dest $_) }
 
     # Install packages
-    Write-Output "► Installing packages" -ForegroundColor Cyan
+    Write-Host "► Installing packages" -ForegroundColor Cyan
     winget configure --enable
     $wingets | Foreach-Object {
         winget configure -f (Join-Path $dest $_) --accept-configuration-agreements --disable-interactivity
     }
 
     # Refresh PATH to include user PATH additions without needing to restart the shell
-    Write-Output "► Refreshing PATH" -ForegroundColor Cyan
+    Write-Host "► Refreshing PATH" -ForegroundColor Cyan
     $userPath = Get-EnvVar -Name "PATH" -Scope "User"
     $machinePath = Get-EnvVar -Name "PATH" -Scope "Machine"
     $env:PATH = "$userPath;$machinePath"
@@ -49,6 +54,6 @@ if (Get-Command -Name winget -ErrorAction SilentlyContinue) {
     chezmoi init --apply $GITHUB_USERNAME
 }
 else {
-    Write-Output "winget is not installed. Please install winget first." -ForegroundColor Red
+    Write-Host "winget is not installed. Please install winget first." -ForegroundColor Red
     exit 1
 }
