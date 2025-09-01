4019a991cd5533a92057263cf924812816a3e5f4
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-09-01 19:48:20 +0200

    Suppress output during winget self-upgrade and configure WinRM transport to SSL in Vagrantfile

--- install.ps1
+++ install.ps1
@@ -20,7 +20,7 @@ if (Get-Command -Name winget -ErrorAction SilentlyContinue) {
     
     #Self upgrade winget
     Write-Host "► Upgrading winget" -ForegroundColor Cyan
-    winget upgrade winget | Out-Null
+    winget upgrade winget --accept-source-agreements --accept-source-agreements --disable-interactivity | Out-Null
     
     # Install packages    
     $wingets | Foreach-Object {
