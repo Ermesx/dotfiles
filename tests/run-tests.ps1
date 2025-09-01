4df33026aa55c06677c57a95beabc0a9e352c9c9
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-09-01 22:52:23 +0200

    Refactor installation scripts and update Vagrantfile for improved PATH handling and test output

--- tests/run-tests.ps1
+++ tests/run-tests.ps1
@@ -11,6 +11,6 @@ Write-Host "Install dotfiles:" -ForegroundColor Cyan
 $script = "https://raw.githubusercontent.com/Ermesx/dotfiles/refs/heads/$BRANCH/install.ps1"
 Invoke-Expression "&{$(Invoke-RestMethod $script)}"
 
-Write-Host "🧪 Running tests..." -ForegroundColor Cyan
+Write-Host "► Running tests" -ForegroundColor Cyan
 pwsh -NoProfile -NoLogo -ExecutionPolicy Bypass -File "C:\vagrant\execute-pester.ps1" 
 
