92c657d254f2a3942df5dbd21e32071905bfe733
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-09-12 18:08:39 +0200

    better working psfzf

--- home/dot_profile/psfzf_profile.ps1
+++ home/dot_profile/psfzf_profile.ps1
@@ -1,4 +1,9 @@
-Set-PsFzfOption -TabExpansion
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' `
-                -PSReadlineChordReverseHistory 'Ctrl+h'
+Set-PsFzfOption -TabExpansion `
+                -GitKeyBindings `
+                -EnableFd `
+                -PSReadlineChordProvider 'Ctrl+t' `
+                -PSReadlineChordReverseHistory 'Ctrl+r' `
+                -PSReadlineChordSetLocation 'Alt+c' `
+                -AltCCommand { param($Location) z $Location }
+                
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion } -BriefDescription "Run fzf Tab completion"
