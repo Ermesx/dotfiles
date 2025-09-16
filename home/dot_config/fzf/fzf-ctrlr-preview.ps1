2fcb100ddafa1d005648dbf0a14c92781cb88a43
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-09-16 09:15:22 -0700

    Improve fzf and add yazi (#8)
    * moctly cleanup and correcting bindings
    
    * add missing env setting
    
    * better working psfzf
    
    * Add fzf configuration files and update environment variables
    
    * Update fzf options and add Ctrl+s key binding for content search
    
    * Add yazi function to streamline directory navigation and include sxyazi.yazi in packages
    
    * Add Get-FlatContent removed function to retrieve and flatten file content
    
    * Update fzf default options for improved usability and aesthetics

---
+++ home/dot_config/fzf/fzf-ctrlr-preview.ps1
@@ -0,0 +1,20 @@
+# Script for Ctrl+R to search command history with fzf and show help for selected command
+param([string] $Line)
+
+$cmd = (($Line -replace "`r?`n", '').TrimStart() -split '\s+')[0]
+if ([string]::IsNullOrWhiteSpace($cmd)) {
+    return
+}
+
+if ($cmd.StartsWith('$')) {
+    Invoke-Expression $cmd 2>&1 | Out-String
+    return
+}
+
+$g = Get-Command -Name $cmd -ErrorAction SilentlyContinue | Select-Object -First 1
+if ($g -and $g.CommandType -ne 'Application') {
+    Get-Help $cmd -ErrorAction SilentlyContinue | bat -l man --style=plain | Out-String
+}
+else {
+    & $cmd --help 2>&1 | bat -l man --style=plain | Out-String
+}
--- home/dot_profile/psfzf_profile.ps1
+++ home/dot_profile/psfzf_profile.ps1
@@ -1,4 +1,10 @@
-Set-PsFzfOption -TabExpansion
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' `
-                -PSReadlineChordReverseHistory 'Ctrl+h'
+Set-PsFzfOption -TabExpansion `
+                -GitKeyBindings `
+                -EnableFd `
+                -PSReadlineChordProvider 'Ctrl+t' `
+                -PSReadlineChordReverseHistory 'Ctrl+r' `
+                -PSReadlineChordSetLocation 'Alt+c' `
+                -AltCCommand { param($Location) z $Location }
+                
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion } -BriefDescription "Run fzf Tab completion"
+Set-PSReadLineKeyHandler -Key Ctrl+s -ScriptBlock { Invoke-PsFzfRipgrep -SearchString '' } -BriefDescription "Run fzf fils content search"
