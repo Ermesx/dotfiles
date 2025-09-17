57e8ef375d9d34f9a95f55c9ea10b822871f001d
Wojciech Gadziński <wojtekgadzinski@gmail.com>
2025-09-17 22:29:29 +0200

    Add dotnet tools to packages.yaml and update DSC configuration

--- home/.chezmoiscripts/windows/run_once_before_00-pre-dsc.ps1
+++ home/.chezmoiscripts/windows/run_once_before_00-pre-dsc.ps1
@@ -6,5 +6,6 @@ if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
 
 Write-Host "► Installing Requred DSC Resources" -ForegroundColor Cyan
 Install-Module PowerShellModule
+Install-Module Microsoft.DotNet.Dsc -AllowPrerelease
 Install-Module Microsoft.Windows.Settings -AllowPrerelease
 Install-Module Microsoft.Windows.Developer -AllowPrerelease -AllowClobber
