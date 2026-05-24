ae944d36d8d3e7b0887dc24b112bfdae27ee1925
Wojciech Gadziski <wojtekgadzinski@gmail.com>
2026-05-24 18:57:54 +0200

    Add instaling winget instead of exit

--- install.ps1
+++ install.ps1
@@ -3,34 +3,40 @@ param (
     [string]$GITHUB_USERNAME = 'Ermesx'
 )
 
+$params = @(
+    '--accept-source-agreements'
+    '--accept-package-agreements'
+    '--disable-interactivity'
+)
+
 if (Get-Command -Name winget -ErrorAction SilentlyContinue) {
-    $params = @(
-        '--accept-source-agreements'
-        '--accept-source-agreements'
-        '--disable-interactivity'
-    )
 
     # Self upgrade winget
     Write-Host '► Upgrading winget' -ForegroundColor Cyan
     winget upgrade winget @params| Out-Null
-
-    # Required packages to install
-    $wingets = @('twpayne.chezmoi', 'microsoft.dsc', 'microsoft.powershell')
-
-    $wingets | Foreach-Object {
-        Write-Host "► Installing $_" -ForegroundColor Cyan
-        winget install $_ @params | Out-Null
-    }
-
-    # Refresh PATH without needing to restart the shell
-    $userPath = [System.Environment]::GetEnvironmentVariable('PATH', 'User')
-    $machinePath = [System.Environment]::GetEnvironmentVariable('PATH', 'Machine')
-    $env:PATH = "$userPath;$machinePath"
-
-    # Install chezmoi and apply dotfiles
-    chezmoi init --apply $GITHUB_USERNAME
 }
 else {
-    Write-Host 'winget is not installed. Please install winget first.' -ForegroundColor Red
-    exit 1
+    
+    # Install winget
+    Write-Host '► Installing winget' -ForegroundColor Cyan
+    $appInstallerPath = "$env:TEMP\Microsoft.DesktopAppInstaller.msixbundle"
+
+    Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile $appInstallerPath -UseBasicParsing
+    Add-AppxPackage -Path $appInstallerPath -ForceApplicationShutdown -ForceUpdateFromAnyVersion -Confirm:$false
 }
+
+# Required packages to install
+$wingets = @('twpayne.chezmoi', 'microsoft.dsc', 'microsoft.powershell')
+
+$wingets | Foreach-Object {
+    Write-Host "► Installing $_" -ForegroundColor Cyan
+    winget install $_ @params | Out-Null
+}
+
+# Refresh PATH without needing to restart the shell
+$userPath = [System.Environment]::GetEnvironmentVariable('PATH', 'User')
+$machinePath = [System.Environment]::GetEnvironmentVariable('PATH', 'Machine')
+$env:PATH = "$userPath;$machinePath"
+
+# Install chezmoi and apply dotfiles
+chezmoi init --apply $GITHUB_USERNAME
